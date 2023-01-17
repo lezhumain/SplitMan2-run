737b4b8177c6ac43913155c753be2b13281de809
Julien Merion <julien.merion@whitespace.co.uk>
2023-01-17 22:32:15 +0000

    Create setup_azure_ssh.sh

---
+++ setup_azure_ssh.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# add ssh in config
+FILE="$HOME/.ssh/config"
+tee -a "$FILE" > /dev/null <<EOT
+Host ovhVM_rel
+  HostName "$(DEBIAN_IP)"
+  User "$(DEBIAN_USER)"
+  IdentityFile "$HOME/.ssh/id_releaseUser"
+  Port 22
+  PreferredAuthentications publickey
+EOT
+
+# add key to agent ?
+eval "$(ssh-agent -s)" && ssh-add "$HOME/.ssh/id_releaseUser"
+echo "Added key to agent"
+
+if [ ! -f "$HOME/.ssh/known_hosts" ]; then
+  touch "$HOME/.ssh/known_hosts"
+fi
+ssh-keyscan -t rsa 79.137.33.77 >> "$HOME/.ssh/known_hosts"
+cat "$HOME/.ssh/known_hosts"
