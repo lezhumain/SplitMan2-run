891a462fefb844493ac5c30e296b46111fe7f667
Julien Merion <julien.merion@whitespace.co.uk>
2023-01-30 00:39:30 +0000

    upload docker compose

--- run_containers.sh
+++ run_containers.sh
@@ -6,6 +6,21 @@ fi
 
 scp ./waitForServer.sh "ovhVM_rel:${DEBIAN_PATH}"
 
+echo "MONGO CREDS: $MONGO_USER - $MONGO_PASS"
+if [ -z "$MONGO_USER" ] || [ -z "$MONGO_PASS" ]; then
+  echo "Need to export MONGO creds."
+  exit 1
+fi
+
+COMPOSE_FILE="docker-compose.yml"
+if [ ! -f "$COMPOSE_FILE" ]; then
+  COMPOSE_FILE="../SplitMan2-run/docker-compose.yml"
+fi
+sed -i.bak -E "s|MONGO_INITDB_ROOT_USERNAME=.+$|MONGO_INITDB_ROOT_USERNAME=$MONGO_USER|" "$COMPOSE_FILE"
+sed -i.bak -E "s|MONGO_INITDB_ROOT_PASSWORD=.+$|MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASS|" "$COMPOSE_FILE"
+
+scp "./$COMPOSE_FILE" "ovhVM_rel:${DEBIAN_PATH}"
+
 ssh -oBatchMode=yes "ovhVM_rel" bash << EOF
   cd "${DEBIAN_PATH}"
   chmod +x ./*.sh
