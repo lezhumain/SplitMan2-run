edbf0b32f23fa6dbd8cba997dd7b8a7433fcbd97
lezhumain <julien.merion@whitespace.co.uk>
2023-05-04 21:33:58 +0100

    vars in specific stuff

--- run_docker_e2e.sh
+++ run_docker_e2e.sh
@@ -6,14 +6,16 @@ WORKING_DIR="$(pwd)"
 USE_HTTPS="$1"
 TARGET_REPOS="$2"
 
-if [ -z "$HOST_IP" ]; then
-       ## EXT ip
-       ##HOST_IP="$(curl -s https://api.myip.com | jq -r .ip)"
-       ## LOCAL ip
-       #HOST_IP="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
-       echo "HOST_IP is not defined."
-       exit 1
-fi
+function check_host_var() {
+    if [ -z "$HOST_IP" ]; then
+           ## EXT ip
+           ##HOST_IP="$(curl -s https://api.myip.com | jq -r .ip)"
+           ## LOCAL ip
+           #HOST_IP="$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)"
+           echo "HOST_IP is not defined."
+           exit 1
+    fi
+}
 
 #docker system prune -af
 
@@ -53,40 +55,50 @@ function update_dir()
 #export MONGO_USER="SECRET_TODO"
 #export MONGO_PASS="SECRET_TODO"
 
-# TODO move this in API part
-echo "MONGO CREDS: $MONGO_USER - $MONGO_PASS"
-if [ -z "$MONGO_USER" ] || [ -z "$MONGO_PASS" ]; then
-  echo "Need to export MONGO creds."
-  exit 1
-fi
-##
-
-# TODO do this in release
-COMPOSE_FILE="docker-compose.yml"
-if [ ! -f "$COMPOSE_FILE" ]; then
-  COMPOSE_FILE="../SplitMan2-run/docker-compose.yml"
-fi
-sed -i.bak -E "s|MONGO_INITDB_ROOT_USERNAME=.+$|MONGO_INITDB_ROOT_USERNAME=$MONGO_USER|" "$COMPOSE_FILE"
-sed -i.bak -E "s|MONGO_INITDB_ROOT_PASSWORD=.+$|MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASS|" "$COMPOSE_FILE"
-##
-
 for REPO in $TARGET_REPOS
 do
   echo "$REPO"
 
   if [ "$REPO" == "SplitMan2-nginx" ]; then
+    check_host_var
+
     export CERT_PATH="$HOME/.ssl/letsencrypt" # HIDE this?
     update_dir ".." "SplitMan2-nginx" "main"
     #cd ../SplitMan2-nginx
     bash doBuild.sh "http://$HOST_IP" # nginx
   elif [ "$REPO" == "SplitMan2-API" ]; then
+    check_host_var
+
+    # echo "MONGO CREDS: $MONGO_USER - $MONGO_PASS"
+    if [ -z "$MONGO_USER" ] || [ -z "$MONGO_PASS" ]; then
+      echo "Need to export MONGO creds."
+      exit 1
+    fi
+
     update_dir ".." "SplitMan2-API" "master"
     #cd ../SplitMan2-API
     bash doBuild.sh "https://$HOST_IP:8081" # api
   elif [ "$REPO" == "SplitMan2" ]; then
+    check_host_var
+
     update_dir ".." "SplitMan2" "master"
     #cd ../SplitMan2
     bash doBuild.sh "https://$HOST_IP:8081" "/api" # web
+  elif [ "$REPO" == "mongo" ]; then
+    # echo "MONGO CREDS: $MONGO_USER - $MONGO_PASS"
+    if [ -z "$MONGO_USER" ] || [ -z "$MONGO_PASS" ]; then
+      echo "Need to export MONGO creds."
+      exit 1
+    fi
+
+    COMPOSE_FILE="docker-compose.yml"
+    if [ ! -f "$COMPOSE_FILE" ]; then
+      #COMPOSE_FILE="../SplitMan2-run/docker-compose.yml"
+      echo "Could not find compose file"
+      exit 1
+    fi
+    sed -i.bak -E "s|MONGO_INITDB_ROOT_USERNAME=.+$|MONGO_INITDB_ROOT_USERNAME=$MONGO_USER|" "$COMPOSE_FILE"
+    sed -i.bak -E "s|MONGO_INITDB_ROOT_PASSWORD=.+$|MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASS|" "$COMPOSE_FILE"
   else
     echo "Unknown repo $REPO"
   fi
