26be6770546e22bac00c14223fddc6cb8950dc49
Julien Merion <julien.merion@whitespace.co.uk>
2023-06-14 22:50:33 +0100

    use splitman devbop branch for ex devops (temp)

--- run_docker_e2e.sh
+++ run_docker_e2e.sh
@@ -81,7 +81,7 @@ do
   elif [ "$REPO" == "SplitMan2" ]; then
     check_host_var
 
-    update_dir ".." "SplitMan2" "master"
+    update_dir ".." "SplitMan2" "fix_build_devops"
     #cd ../SplitMan2
     bash doBuild.sh "https://$HOST_IP:8081" "/api" # web
   elif [ "$REPO" == "mongo" ]; then
